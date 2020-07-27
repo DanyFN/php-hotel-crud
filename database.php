7b649642ad978fa075a6257003d274b3d861ce94
Daniele <danielefontana90@gmail.com>
2020-07-27 16:31:01 +0200

    Laravel mix + db

---
+++ database.php
@@ -0,0 +1,30 @@
+<?php
+
+  $name = "localhost";
+  $username = "root";
+  $password = "root";
+  $dbname = "boolean-hotel";
+  // Connect
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn && $conn->connect_error) {
+  echo "Connection failed: " . $conn->connect_error;
+  }
+
+
+  $sql = "SELECT * FROM `stanze`";
+  $result = $conn->query($sql);
+
+  if ($result && $result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+  var_dump($row);
+  }
+  } elseif ($result) {
+  echo "0 results";
+  } else {
+  echo "query error";
+  }
+  $conn->close();
+
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include __DIR__ . '/database.php';?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    ciao
+
+  </body>
+</html>
